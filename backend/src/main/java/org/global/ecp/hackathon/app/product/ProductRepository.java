3fd9a576205eeb0a08a7179e98b3415c225e8584
Luke Campbell <luke.campbell@global.com>
2023-06-01 14:52:10 +0100

    chore: task 14

--- backend/src/main/java/org/global/ecp/hackathon/app/product/ProductRepository.java
+++ backend/src/main/java/org/global/ecp/hackathon/app/product/ProductRepository.java
@@ -14,20 +14,6 @@ public class ProductRepository {
 
     public ProductRepository() {
         products = new HashMap<>();
-
-        // TODO - Task 14: remove this method
-        populateProductsMap();
-    }
-
-    // TODO - Task 14: remove this method
-    private void populateProductsMap() {
-
-        Product product1 = new Product(1, "Product 1", "This is a description for product 1", 1.0);
-        Product product2 = new Product(2, "Product 2", "This is a description for product 2", 2.0);
-        Product product3 = new Product(3, "Product 3", "This is a description for product 3", 3.0);
-        products.put(product1.getId(), product1);
-        products.put(product2.getId(), product2);
-        products.put(product3.getId(), product3);
     }
 
     public List<Product> getAll() {
