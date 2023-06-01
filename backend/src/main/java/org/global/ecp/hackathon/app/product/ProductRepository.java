aefda9fef96f42d8ec6e6f5d9d6341d364209c2f
Luke Campbell <luke.campbell@global.com>
2023-06-01 16:12:32 +0100

    chore: adding products

--- backend/src/main/java/org/global/ecp/hackathon/app/product/ProductRepository.java
+++ backend/src/main/java/org/global/ecp/hackathon/app/product/ProductRepository.java
@@ -14,6 +14,19 @@ public class ProductRepository {
 
     public ProductRepository() {
         products = new HashMap<>();
+        populateProductsMap();
+    }
+
+    private void populateProductsMap() {
+
+        Product product1 = new Product(1, "Colour Blast n Slay", "Vibrant with new life", 22.00);
+        Product product2 = new Product(2, "Supreme Slay n Stay", "Hair for a long time... and a good time", 35.99);
+        Product product3 = new Product(3, "Pump n Slay", "Compact and portable for your travels", 14.99);
+        Product product4 = new Product(4, "Bush n Slay", "This is definitely for your facial hair", 19.00);
+        products.put(product1.getId(), product1);
+        products.put(product2.getId(), product2);
+        products.put(product3.getId(), product3);
+        products.put(product4.getId(), product4);
     }
 
     public List<Product> getAll() {
