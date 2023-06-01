982bbeec42de3a95791dd70feb01e9c4a27b0a4a
Gavin <gavin.sandford@global.com>
2023-06-01 11:44:47 +0100

    task 7 shipping and total cost

--- frontend/src/components/checkout/CheckoutSummary.tsx
+++ frontend/src/components/checkout/CheckoutSummary.tsx
@@ -19,22 +19,22 @@ function CheckoutSummary({setShowCheckoutSummary}: { setShowCheckoutSummary: Rea
     // If the total cost of items was £10, the shipping would be £1
     // Also if the total cost of items is over or equal to £50, the shipping costs are free or worth £0
     function getShippingCost(totalCostOfItems: number) {
-
         let shippingCost = 0.0000;
-        return roundToTwoDecimalPlaces(shippingCost);
+        if (totalCostOfItems > 50) return shippingCost;
+        shippingCost = totalCostOfItems / 10;
+        return shippingCost;
     }
 
     // TODO - Task 7: calculate the total cost of items and shipping
     function getTotalCost(totalCostOfItems: number, shippingCost: number) {
-
-        let totalCost = 0.0000;
-        return roundToTwoDecimalPlaces(totalCost);
+        return totalCostOfItems + shippingCost;
     }
 
     // This function will round any number to two digits
     // If you have the number "0.01234" you will get "0.01"
-    function roundToTwoDecimalPlaces(num: number) {
-        return +(Math.round(Number(num + "e+2")) + "e-2");
+    function formatAsGBP(num: number) {
+        const formatter = new Intl.NumberFormat('en-UK', {style: 'currency', currency: 'GBP'})
+        return formatter.format(num);
     }
 
     function checkoutBasket() {
@@ -45,9 +45,9 @@ function CheckoutSummary({setShowCheckoutSummary}: { setShowCheckoutSummary: Rea
     return (
         <div className="checkout-summary-container">
             <div className="checkout-summary">
-                <label>Subtotal - £ {totalCostOfItems}</label>
-                <label>Shipping cost - £ {shippingCost}</label>
-                <label>Total cost - £ {getTotalCost(totalCostOfItems, shippingCost)}</label>
+                <label>Subtotal - {formatAsGBP(totalCostOfItems)}</label>
+                <label>Shipping cost - {formatAsGBP(shippingCost)}</label>
+                <label>Total cost - {formatAsGBP(getTotalCost(totalCostOfItems, shippingCost))}</label>
                 <div className="checkout-summary-buttons">
                     <div className="checkout-summary-button" onClick={() => setShowCheckoutSummary(false)}>Go Back</div>
                     <div className="checkout-summary-button" onClick={() => checkoutBasket()}>Complete Checkout</div>
